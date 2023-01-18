98a2141311671ebb5b99bb4df41e491aa9392a32
Antônio Carlos <juniorbecari@gmail.com>
2023-01-18 17:28:38 -0300

    Finished data structures

--- data-structures.go
+++ data-structures.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+type Any interface {}
+
+type DataStruct interface {
+  func Get(index int) Any
+  func Len() int
+  
+}
+
 type Stack struct {
   value []Any
 }
@@ -9,6 +19,10 @@ type Queue struct {
 }
 
 // --- //
+
+func NewStack(values []Any) *Stack {
+  return &Stack { values }
+}
 
 func (s *Stack) Push(v Any) {
   s.value = append(s.value, v)
@@ -31,25 +45,37 @@ func (s *Stack) Get(index int) Any {
   return s.value[index]
 }
 
+func (s *Stack) Len() int {
+  return len(s.value)
+}
+
 // --- //
 
+func NewQueue(values []Any) *Queue {
+  return &Queue { values }
+}
+
 func (q *Queue) Enqueue(v Any) {
-  s.value = append(s.value, v)
+  q.value = append(q.value, v)
 }
 
 func (q *Queue) Dequeue() Any {
-  ret := s.value[len(s.value) - 1]
-  s.value = s.value[:len(s.value) - 1]
+  ret := q.value[0]
+  q.value = q.value[1:]
   
   return ret
 }
 
 func (q *Queue) Poll() Any {
-  ret := s.value[0]
+  ret := q.value[0]
   
   return ret
 }
 
 func (q *Queue) Get(index int) Any {
-  return s.value[index]
+  return q.value[index]
+}
+
+func (q *Queue) Len() int {
+  return len(q.value)
 }
